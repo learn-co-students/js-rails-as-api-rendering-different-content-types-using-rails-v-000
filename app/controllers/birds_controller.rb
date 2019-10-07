0bf419bd4ea997b7b8738a223f2e6e963bce80c8
github username <github email address>
2019-10-06 20:45:04 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,10 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render 'birds/index.html.erb'
+    # render 'birds/index.html.erb'
+    # render json: 'hi birds'
+    # render json: {message: 'hi birds'}
+    # render json: ['hi', 'birds']
+    render jspn: @birds
   end
 end
