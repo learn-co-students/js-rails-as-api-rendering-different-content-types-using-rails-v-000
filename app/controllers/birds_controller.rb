4e1c87e45a9a3759de700bbe2e4085377bb91185
Mohammed Noori <kmanoorib@gmail.com>
2019-12-27 04:26:00 +0000

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,10 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    # @birds = Bird.all
+    # render 'birds/index.html.erb'
+    # how to render above going forward. no longer have to deal with instance variables because
+    # we are directly rendering to JSON in the same action
+    birds = Bird.all
+    render json: birds
   end
 end
