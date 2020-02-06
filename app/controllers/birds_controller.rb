9e6f145d22f581290b7ba7d08e51007c683422c7
William Barela <william.barela.dev@gmail.com>
2020-02-06 15:06:50 -0600

    rendering plain beautiful json!

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,11 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    render json: birds
+    # @birds = Bird.all
+    # render 'birds/index.html.erb'
+    # render plain: @birds.map{|b| b.name }
+    #render json: @birds
+    # render json: { birds: @birds, message: 'Ve like ze colors!'}
   end
 end
