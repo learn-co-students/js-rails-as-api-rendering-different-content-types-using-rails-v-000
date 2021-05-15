ee6092c6c41b8a5ac6223241b50de6882391b764
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-14 20:17:12 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,9 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    # instance variable not required since all handling happens within the action
+    birds = Bird.all
+    # render 'birds/index.html.erb'
+    # render plain: "Hello #{@birds[3].name}"
+    render json: birds
   end
 end
